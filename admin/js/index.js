5aac798fe72bdc721d9f8b002df182997b628664
springhack <springhack@live.cn>
2023-01-31 23:52:10 +0800

    try new login method

--- admin/js/index.js
+++ admin/js/index.js
@@ -1,18 +1,11 @@
 /*
  *  Author: SpringHack - springhack@live.cn
- *  Last modified: 2023-01-18 23:29:54
- *  Filename: index.js
+ *  Last modified: 2023-02-01 00:02:26
+ *  Filename: admin/js/index.js
  *  Description: Created by SpringHack using vim automatically.
  */
 // Inject github markdown css
 CMS.registerPreviewStyle('/admin/css/markdown.css');
-
-// Hook login phase
-let loginButton = null;
-let loginForm = null;
-let changed = false;
-let timer = null;
-let frame = null;
 
 // Login button click
 document.addEventListener('click', (ev) => {
@@ -21,80 +14,16 @@ document.addEventListener('click', (ev) => {
       ev.target.className.includes('LoginButton')) {
     // Stop propagation right now
     ev.stopImmediatePropagation();
-    if (changed) {
-      return;
-    }
-    // Open window for auth
-    const frame = document.createElement('iframe');
-    frame.src = './passport.html';
-    frame.className = 'passport-frame';
-    frame.referrerPolicy = 'no-referrer';
-    document.body.appendChild(frame);
-    window.addEventListener('message', (event) => {
-      if (event.data && event.data.type === 'passport') {
-        const {token} = event.data;
-        const netlifyCmsUser = {token, backendName: 'github'};
-        localStorage.setItem(
-            'netlify-cms-user', JSON.stringify(netlifyCmsUser));
-        location.reload();
-      }
-    });
+    const url = new URL(location.href);
+    const redirectUri = `${url.origin}${url.pathname}`;
+    const newUrl = new URL('https://github.com/login/oauth/authorize');
+    newUrl.searchParams.set('scope', 'public_repo');
+    newUrl.searchParams.set('redirect_uri', redirectUri);
+    newUrl.searchParams.set('client_id', globalThis.oauthInfo.client_id);
+    location.href = newUrl.toString();
   }
   // Oops...
   return;
-}, true);
-
-// Login button long press start
-document.addEventListener('mousedown', (ev) => {
-  if (ev && ev.target && ev.target.className && ev.target.className.includes &&
-      ev.target.className.includes('LoginButton')) {
-    timer = setTimeout(() => {
-      changed = true;
-      // Exchange UI
-      loginButton = ev.target;
-      loginButton.style.visibility = 'hidden';
-      loginForm = document.createElement('form');
-      loginButton.parentElement.insertBefore(loginForm, loginButton);
-      // Token input element
-      let input = document.createElement('input');
-      input.autocomplete = 'current-password';
-      input.placeholder = 'wtf does the fuck say';
-      input.autofocus = true;
-      input.type = 'password';
-      input.addEventListener('keydown', (ev) => {
-        if (ev.key === 'Enter') {
-          const token = input.value;
-          const netlifyCmsUser = {token, backendName: 'github'};
-          localStorage.setItem(
-              'netlify-cms-user', JSON.stringify(netlifyCmsUser));
-          location.reload();
-        }
-      });
-      input.style.width = '300px';
-      input.style.padding = '7px';
-      input.style.fontSize = '15px';
-      input.style.textAlign = 'center';
-      input.style.borderRadius = '20px';
-      loginForm.appendChild(input);
-      input.focus();
-      // Login form
-      loginForm.style.top = '-38px';
-      loginForm.style.height = '0px';
-      loginForm.style.position = 'relative';
-    }, 2000);
-  }
-}, true);
-
-// Login button long press end
-document.addEventListener('mouseup', (ev) => {
-  clearTimeout(timer);
-  // Ignore other click events
-  if (changed && ev && ev.target && ev.target.className &&
-      ev.target.className.includes &&
-      ev.target.className.includes('LoginButton')) {
-    // Stop propagation right now
-    ev.stopImmediatePropagation();
-  }
 }, true);
 
 // Custom widget for filename
